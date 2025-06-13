db0b2765dd3b967b515fb1958a4d57a8322979d1
Thomas Reppesgård <thomas.reppesgard@digdir.no>
2025-06-13 14:14:17 +0200

    EUW-271: Laste keystore for token-signering (#9)

--- src/main/java/no/idporten/eudiw/oauthserver/crypto/KeyStoreProvider.java
+++ src/main/java/no/idporten/eudiw/oauthserver/crypto/KeyStoreProvider.java
@@ -1,8 +1,9 @@
 package no.idporten.eudiw.oauthserver.crypto;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.context.ApplicationContextException;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,6 +11,7 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+import java.util.Base64;
 
 /**
  * Utility for loading a keystore using a resource loader.
@@ -19,17 +21,34 @@ public class KeyStoreProvider {
 
     private KeyStore keyStore;
 
-    public KeyStoreProvider(String type, String location, String password, ResourceLoader resourceLoader) {
-        try (InputStream is = new FileInputStream(resourceLoader.getResource(location).getFile())) {
-            KeyStore keyStore = KeyStore.getInstance(type);
-            keyStore.load(is, password.toCharArray());
+    public KeyStoreProvider(KeyStoreProperties keyStoreProperties) {
+        try (InputStream is = inputStreamForLocation(keyStoreProperties.location())) {
+            KeyStore keystore = KeyStore.getInstance(keyStoreProperties.type());
+            keystore.load(is, keyStoreProperties.password().toCharArray());
             if (log.isInfoEnabled()) {
-                log.info("Loaded keystore of type {} from {}", type, location);
+                log.info("Loaded keystore of type {} from {}",
+                        keyStoreProperties.type(),
+                        keyStoreProperties.location().startsWith("base64:")
+                                ? String.format("%100.100s...", keyStoreProperties.location())
+                                : keyStoreProperties.location());
             }
-            this.keyStore = keyStore;
+            this.keyStore = keystore;
         } catch (IOException | KeyStoreException | CertificateException | NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
+            throw new ApplicationContextException("Failed to load keystore.", e);
         }
+    }
+
+    private InputStream inputStreamForLocation(String location) throws IOException {
+        if (location.startsWith("base64:")) {
+            return new ByteArrayInputStream(Base64.getDecoder().decode(location.substring(7)));
+        }
+        if (location.equals("file:")) {
+            return new FileInputStream(location.substring(5));
+        }
+        if (location.startsWith("classpath:")) {
+            return this.getClass().getClassLoader().getResourceAsStream(location.substring(10));
+        }
+        throw new IOException("Unable to load keystore from location [%s]".formatted(location));
     }
 
     public KeyStore keyStore() {
